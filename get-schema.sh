12cbc03079123b311405f8028d7ce9fea8489345
satanson <ranpanf@gmail.com>
2019-09-24 07:54:54 +0800

    by satanson

--- get-schema.sh
+++ get-schema.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-t=$1
-echo "!desc $t;" > $t.1
-./sqlline | sed 's#\[1;32m | \[m##g' | sed '/---/d'  | sed '/TABLE_CAT/d' | sed 's#[1;32m##g' | sed /d'| sed 's#|##g' | awk '{if ($0 ~"PK ") {if (NR != 1) print ")  compression = '\''snappy'\'', UPDATE_CACHE_FREQUENCY=30000, COLUMN_ENCODED_BYTES=0;"; print "create table if not exists "$2"."$3" (\n\"PK\"  VARCHAR PRIMARY KEY"; } else {print ",\""$4"\" ", $6}; }''END{print ")  compression = '\''snappy'\'', UPDATE_CACHE_FREQUENCY=30000, COLUMN_ENCODED_BYTES=0;"}' | sed 's#[m##g'  > $t.sql
-rm $t.1
+basedir=$(cd $(dirname $(readlink -f ${BASH_SOURCE:-$0}));pwd)
+cd ${basedir}
+
+t=${1:?"missing 't'"};shift
+
+echo "!desc $t" | ./sqlline | \
+       perl -lpe 's#(\x{1b}\[1;32m\s*)|(\s*\x{1b}\[m)##g' | \
+       perl -lne 'print unless /(---)|(TABLE_CAT)/' | \
+       perl -lne 'print unless /\x{08}\x{08}/' | \
+       perl -lne 'print unless /jdbc:phoenix/' | \
+       perl -pe 's#\|##g' | \
+       perl -aF'\s+' -lne '
+        if (/\ID\b/) {
+          if($. != 1 ){
+            print qq#) compression=\x{27}snappy\x{27},UPDATE_CACHE_FREQUENCY=30000, COLUMN_ENCODED_BYTES=0;#
+          };
+          print qq#create table if not exists $F[1].$F[2] (\n"PK" VARCHAR PRIMARY KEY#
+        } else {
+          print qq#,"$F[3]" $F[5]#
+        }
+        END {
+          print qq#) compression=\x{27}snappy\x{27},UPDATE_CACHE_FREQUENCY=30000, COLUMN_ENCODED_BYTES=0;#
+        }' |tee ${t}.sql
