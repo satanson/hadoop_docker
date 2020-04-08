2d7d8dd883a084acff4b7f58be2d1a30a9b86cb7
satanson <ranpanf@gmail.com>
2020-04-08 20:34:12 +0800

    add start_all.sh

---
+++ start_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+bootstrap=$1
+
+set -e -o pipefail
+basedir=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+cd ${basedir}
+
+${basedir}/start_zk.sh ${bootstrap}
+sleep 5
+${basedir}/start_bk.sh ${bootstrap}
+sleep 5
+${basedir}/start_hdfs.sh ${bootstrap}
+sleep 5
+${basedir}/start_yarn.sh ${bootstrap}
+sleep 5
+${basedir}/start_mr_history_server.sh
+${basedir}/start_spark_history_server.sh
